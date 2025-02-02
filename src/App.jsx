a151048828030d226957547ef23aa7998676baec
Hwang Jiwon <jiw2703@naver.com>
2025-02-02 20:48:43 +0900

    Feat :네비게이션바 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Nav from './components/Nav';
 import HomePage from './pages/HomePage';
 import ClassMainPage from './pages/ClassMainPage';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className='App-wrapper'>
+        <Nav/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="class-main-page" element={<ClassMainPage />} />
---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import {Link, useNavigate} from 'react-router-dom';
+import './Nav.css';
+import Logo from '../assets/images/logo.png';
+
+function Nav() {
+  return (
+    <div>
+      <div className='navbar'>
+        <Link className='logo' to={'/'}>  
+          <img src={Logo} alt='Logo' />
+        </Link>
+        <Link className='navbarMenu' to={'/'}>홈</Link>
+        <Link className='navbarMenu' to={'/class-main-page'}>클래스</Link>
+        <Link className='navbarMenu' to={'/'}>커뮤니티</Link>
+        <Link className='navbarMenu' to={'/'}>테스트</Link>
+        <Link className='navbarMenu' to={'/'}>유료서비스</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Nav;
