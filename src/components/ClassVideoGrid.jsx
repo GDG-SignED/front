cf3b399918635d5765da4d6406159ca5305c2a1c
Hwang Jiwon <jiw2703@naver.com>
2025-02-02 20:56:54 +0900

    Feat #1: 클래스 비디오 링크 데이터, 비디오 그리드 세팅

---
+++ src/components/ClassVideoGrid.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./VideoGrid.css"; 
+
+const VideoGrid = ({ videos }) => {
+  return (
+    <div className="video-grid">
+      {videos.map((video, index) => (
+        <div className="video-container" key={index}>
+          <video controls>
+            <source src={video.url} type="video/mp4" />
+          </video>
+          <p className="video-title">{video.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default VideoGrid;
