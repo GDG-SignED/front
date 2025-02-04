cb8747a6f3d1bccdd6740819112ee20b167ee92f
Hwang Jiwon <jiw2703@naver.com>
2025-02-04 20:08:18 +0900

    Feat #1: 자음모음 영상 링크 추가

--- src/assets/ClassVideoData/ClassConsonants.jsx
+++ src/assets/ClassVideoData/ClassConsonants.jsx
@@ -4,7 +4,15 @@ const videoLinksConsonants = [
   { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220811/1009752/MOV000360049_320X240.mp4", title: "ㄷ (디귿)" },
   { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220802/1005858/MOV000359860_320X240.mp4", title: "ㄹ (리을)" },
   { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220811/1009748/MOV000360045_320X240.mp4", title: "ㅁ (미음)" },
-  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220803/1006271/MOV000359919_320X240.mp4", title: "ㅂ (비읍)" }
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220803/1006271/MOV000359919_320X240.mp4", title: "ㅂ (비읍)" },
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220802/1005893/MOV000359895_320X240.mp4", title: "ㅅ (시옷)" },
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220811/1009745/MOV000360042_320X240.mp4", title: "ㅇ (이응)" },
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220727/1001950/MOV000359527_320X240.mp4", title: "ㅈ (지읒)" },
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220802/1005761/MOV000359838_320X240.mp4", title: "ㅊ (치읓)" },
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220811/1009754/MOV000360051_320X240.mp4", title: "ㅋ (키읔)" },
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220802/1005763/MOV000359840_320X240.mp4", title: "ㅌ (티읕)" },
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220802/1005870/MOV000359872_320X240.mp4", title: "ㅍ (피읖)" },
+  { url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20220802/1005694/MOV000359819_320X240.mp4", title: "ㅎ (히읗)" }
 ];
 
 export default videoLinksConsonants;
