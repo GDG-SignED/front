1c5643d3e940aac10daba78270893e35c54d6ee9
Hwang Jiwon <jiw2703@naver.com>
2025-02-19 19:26:31 +0900

    Remove: 사용안하는 이미지, 파일 삭제

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,8 @@
 import './css/Intro.css';
-import background from '../assets/images/introImage.jpg';
 
 function Intro(){
     return(
-        <div className="intro-container" style={{ backgroundImage: {background} }}>
+        <div className="intro-container">
             <h1 className="title">수어 교육 플랫폼, 손이랑</h1>
             <h2 className="subTitle">AI 기술과 함께 수어를 학습해보세요</h2>
             <button className="loginButton">login</button>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,11 +9,12 @@ function Nav() {
         <Link className='logo' to={'/'}>  
           <img src={Logo} alt='Logo' className='logo-img' />
         </Link>
-        <Link className='navbarMenu' to={'/'}>홈</Link>
         <Link className='navbarMenu' to={'/class-main-page'}>클래스</Link>
         {/* <Link className='navbarMenu' to={'/'}>커뮤니티</Link> */}
         <Link className='navbarMenu' to={'/test-main-page'}>테스트</Link>
         <Link className='navbarMenu' to={'/'}>유료서비스</Link>
+        <Link className='navbarMenu' to={'/'}>로그인/회원가입</Link>
+
       </div>
     </div>
   )
