db1d2d3024087999429b0a97e9a3f55b66be0ed8
Hwang Jiwon <jiw2703@naver.com>
2025-02-19 20:35:00 +0900

    Feat #6: 각 페이지 버튼에 로그인페이지 연결

--- src/pages/ClassMainPage.jsx
+++ src/pages/ClassMainPage.jsx
@@ -4,17 +4,23 @@ import videoLinksConsonants from "../assets/ClassVideoData/ClassConsonants";
 import videoLinksVowels from "../assets/ClassVideoData/ClassVowels";
 import ClassPageContent from "../components/ClassPageContent";
 import { useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function ClassMainPage() {
-  const [selectedCategory, setSelectedCategory] = useState(null);
-
   // 사이드바 메뉴
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const categories = [
     { name: "문화와 역사", key: "history", videos: [] },
     { name: "자음", key: "consonants", videos: videoLinksConsonants },
     { name: "모음", key: "vowels", videos: videoLinksVowels },
     { name: "기초 단어", key: "basicWords", videos: [] },
   ];
+
+  // 로그인 페이지 연결
+  const navigate = useNavigate();
+  function gotoLogin() {
+    navigate('/login-page');
+  }
 
   return (
     <div>
@@ -23,7 +29,7 @@ function ClassMainPage() {
         <header className="class-header">
           <div className="class-left-section">
             <h2>로그인 후에 같이 공부해보아요!</h2>
-            <button className="class-login-button">로그인/회원가입 →</button>
+            <button className="class-login-button" onClick={gotoLogin}>로그인/회원가입 →</button>
           </div>
           <div className="class-right-section">
             {categories.map((category) => (
