073d6b4102a3e42cb59b3a27b50038c7e46de6c4
Hwang Jiwon <jiw2703@naver.com>
2025-02-21 18:11:25 +0900

    Feat #8: 홈화면 인트로 섹션 수정

--- src/components/homepage/Intro.jsx
+++ src/components/homepage/Intro.jsx
@@ -1,5 +1,5 @@
 import './Intro.css';
-import Logo from '../../assets/images/logo.png';
+import Logo from '../../assets/images/homeLogo.png';
 import { Navigate, useNavigate } from 'react-router-dom';
 
 function Intro(){
@@ -9,9 +9,19 @@ function Intro(){
   }
   
   return(
-    <section className="home-intro">
-      <img src={Logo} alt="손이랑 로고" className="home-logo" />
-      <p className="home-subtitle">손이랑과 함께 오늘도 수어를 배워보아요</p>
+    <section className="intro-section">
+      <div className="intro-content">
+        <img src={Logo} alt="손이랑 로고" className="intro-logo" />
+        <p className="intro-subtitle">손이랑과 함께 오늘도 수어를 배워보아요</p>
+      </div>
+
+      {/* 로그인 후
+      <div className="home-user-info">
+        <p className="username">(닉네임) 님 🌱</p>
+        <p className="actions">마이페이지 | 로그아웃</p>
+      </div> */}
+      
+      <button className="home-login-button" onClick={gotoLogin}>로그인/회원가입 →</button>
     </section>
   )
 }
